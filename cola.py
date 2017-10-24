59c86f689cd9a865112a3272d5d9c7094221f11f
Leonel R <leorhcp2030@gmail.com>
2017-10-24 04:31:54 -0300

    Update cola.py

--- cola.py
+++ cola.py
@@ -1,1 +1,39 @@
+# Las colas se usan usualmente
+# /* dar un orden de prioridad a los procesos
+# /* listar cosas
 
+
+class PILA:
+	"""TDA
+		Primitivas
+		/* constructor
+		/* encolar <- value
+		/* desencolar -> value
+		/* ver_primero -> value
+		/* esta_vacia -> bool
+		/* tamaño -> len
+	"""
+	
+	def __init__(self):
+		self.contenedor = []
+	
+	def desencolar(self):
+		if self.esta_vacia():
+			return None
+		
+		return self.contenedor.pop(0)
+	
+	def encolar(self,value):
+		self.contenedor.append(value)
+	
+	def esta_vacia(self):
+		return self.tamanio == 0
+	
+	def ver_primero(self):
+		if self.esta_vacia:
+			return None
+		
+		return self.contenedor[0]
+	
+	def tamanio(self):
+		return len(self.contenedor)
