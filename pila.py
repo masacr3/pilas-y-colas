58797f6462e4a6850661724e352350c8d0c94d50
Leonel R <leorhcp2030@gmail.com>
2017-10-24 03:47:18 -0300

    Update pila.py

--- pila.py
+++ pila.py
@@ -1,11 +1,31 @@
 class PILA:
-	"TDA"
+	"""TDA
+		Primitivas
+		/* constructor
+		/* apilar <- value
+		/* desapilar -> value
+		/* ver_ultimo -> value
+		/* esta_vacia -> bool
+	"""
 	
 	def __init__(self):
 		self.contenedor = []
 	
-	def pop(self):
-		if self.vacia():
+	def desapilar(self):
+		if self.esta_vacia():
 			return None
 		
 		return self.contenedor.pop()
+	
+	def apilar(self,value):
+		self.contenedor.append(value)
+	
+	def esta_vacia(self):
+		return len(self.contenedor) == 0
+	
+	def ver_ultimo(self):
+		if self.esta_vacia:
+			return None
+		
+		return self.contenedor[-1]
+		
