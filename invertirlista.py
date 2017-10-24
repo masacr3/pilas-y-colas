d92d9c26bfb0db6d580df00dfd78943b6f2c0f84
Leonel R <leorhcp2030@gmail.com>
2017-10-24 04:57:31 -0300

    Update invertirlista.py

--- invertirlista.py
+++ invertirlista.py
@@ -1,1 +1,12 @@
+# / invierte una lista mediante TDA pila
+lista = [1,2,3,4,5]
+
+mipila = PILA()
+
+for n in lista:
+	mipila.apila(n)
+
+for i in range(mipila.tamanio()):
+	print(mipila.desapila())
+
 
