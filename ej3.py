4a50b981ff358ae9d040571eb842cd7938c47f9a
Leonel R <leorhcp2030@gmail.com>
2017-10-25 20:43:55 -0300

    Update ej3.py

--- ej3.py
+++ ej3.py
@@ -1,1 +1,27 @@
+# TDA pila
+# con todas sus primitivas
+# /* parcialito 3 
 
+def esta_(cola,elemento):
+	if cola.esta_vacia():
+		return False
+	
+	esta = False
+	_cola = Cola()
+	
+	while not cola.esta_vacia():
+		value = cola.desenconlar()
+		if value == elemento:
+			esta = True
+		
+		_cola.encolar(value)
+		
+		if esta:
+			continue
+			
+	while not _cola.esta_vacia():
+		cola.encolar(_cola.desencolar())
+	
+	return esta
+		
+	
